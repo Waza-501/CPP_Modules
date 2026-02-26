5c336ceec66ba6c6a98c26c35da7fdccbb1760ad
Owen <you@example.comowen.d.hearn@gmail.com>
2026-02-26 13:50:27 +0100

    fragtrap needed.

---
+++ cpp03/ex02/ClapTrap.hpp
@@ -0,0 +1,30 @@
+#ifndef CLAPTRAP_H
+# define CLAPTRAP_H
+
+# include <string>
+
+class ClapTrap
+{
+protected:
+	std::string	name;
+	int			hitpoints;
+	int			energy;
+	int			attackdamage;
+public:
+	ClapTrap();
+	ClapTrap(std::string name);
+	ClapTrap(const ClapTrap& other);
+	ClapTrap& operator=(const ClapTrap& other);
+	~ClapTrap();
+	void		setHitpoints(unsigned int amount);
+	void		setEnergy(unsigned int amount);
+	std::string	getName(void) const;
+	int			getHitpoints(void) const;
+	int			getEnergy(void) const;
+	int			getAttackDamage(void) const;
+	void		attack(const std::string target);
+	void		takeDamage(unsigned int amount);
+	void		beRepaired(unsigned int amount);
+};
+
+#endif
